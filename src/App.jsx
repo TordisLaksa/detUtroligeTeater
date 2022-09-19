4d20e9f7a9399b0af455061d235ccb38fff45a72
TordisLaksa <88707360+TordisLaksa@users.noreply.github.com>
2022-09-19 09:19:07 +0100

    Rettelser i mixins + vars

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './components/App/Router/AppRouter';
 import { Header } from "./components/Partials/Header/Header";
 import { Footer } from "./components/Partials/Footer/Footer";
+import './App.scss'
 
 export const App = () => {
   return (
