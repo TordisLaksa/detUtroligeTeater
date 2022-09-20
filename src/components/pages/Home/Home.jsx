a7310b6feccab8098c1370a47952c8eadb9f1347
TordisLaksa <88707360+TordisLaksa@users.noreply.github.com>
2022-09-20 20:23:00 +0100

    HomeCards

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react"
+import { Link } from "react-router-dom";
 import { ConvertedDate } from "../../App/Helper/Helpers";
 import { Layout } from "../../App/Layout/Layout";
+import './Home.scss';
 
 export const Home = () => {
     
@@ -12,19 +14,20 @@ export const Home = () => {
         
         <Layout title=''>
             <MainHeroEvent />
+            <HighlightedCards />
         </Layout>
         </>
     )
 }
 
 
-const MainHeroEvent = () =>{
+const MainHeroEvent = () => {
     const [ heroEvent, setHeroEvent ] = useState();
     
     useEffect(() => {
         const getHeroEvent = async () => {
             try {
-                const response = await axios.get('https://api.mediehuset.net/detutroligeteater/events?orderby=rand()&limit=1')
+                const response = await axios.get('https://api.mediehuset.net/detutroligeteater/events?limit=1')
                 if (response.data) {
                     setHeroEvent(response.data.items);
                 }
@@ -56,6 +59,53 @@ const MainHeroEvent = () =>{
                 </figure>
                 )    
             })}
+        </section>
+    )
+}
+
+const HighlightedCards = () => {
+    const [ getCardData, setCardData ] = useState();
+    
+    useEffect(() => {
+        const getCards = async () => {
+            try {
+                const response = await axios.get('https://api.mediehuset.net/detutroligeteater/events?orderby=rand()&limit=3')
+                if (response.data) {
+                    setCardData(response.data.items);
+                }
+            } catch (error) {
+                
+            }
+        }
+        getCards()
+    }, [])
+    
+    return(
+        <section>
+            {getCardData && getCardData.map(card => {
+                console.log(card);
+                return(
+                    <figure key={card.id}>
+                        <img src={card.image_medium} alt={card.title} />
+                        <figcaption>
+                            <article>
+                                <p>{card.stage_name}</p>
+                                <h4>{ConvertedDate(card.startdate)} - {ConvertedDate(card.stopdate, true)}</h4>
+                                <hr />
+                            </article>
+                            <article>
+                                <h2>{card.title}</h2>
+                                <h3>{card.genre}</h3>
+                                <div>
+                                    <button><Link to={`/events/${card.id}`}>LÆS MERE</Link></button>
+                                    <button><Link to={`/tickets/${card.id}`}>KØB BILLET</Link></button>
+                                </div>
+                            </article>
+                        </figcaption>
+                    </figure>
+                )
+            })}
+            <button><Link to={'/events'}>SE ALLE FORESTILLINGER</Link></button>
         </section>
     )
 }
